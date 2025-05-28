31e7a9b28f3959a0fd68d66d241b586ebe1cc497
Ramadan1011 <shukhratovich75@gmail.com>
2025-05-28 14:37:41 +0500

    Create response records to respond

---
+++ OnlineMed.Contracts/Responses/Doctor/CreateDoctorResponse.cs
@@ -0,0 +1,17 @@
+﻿using OnlineMed.Domain.Enums;
+
+namespace OnlineMed.Contracts.Responses.Doctor;
+
+public sealed record CreateDoctorResponse(
+    int Id,
+    string FullName,
+    string Email,
+    string PhoneNumber,
+    string Specialization,
+    string ConversationLanguages,
+    int Experience,
+    int Age,
+    decimal Price,
+    decimal Rating,
+    Gender Gender,
+    DateTime CreatedAt);
