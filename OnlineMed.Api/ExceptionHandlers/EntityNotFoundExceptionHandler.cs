6056f5078e26c7d635b7a70ff92f3febe084809e
Ramadan1011 <shukhratovich75@gmail.com>
2025-05-29 15:17:32 +0500

    create exception handlers

---
+++ OnlineMed.Api/ExceptionHandlers/EntityNotFoundExceptionHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OnlineMed.Domain.Exceptions;
+
+namespace OnlineMed.Api.ExceptionHandlers;
+
+internal sealed class EntityNotFoundExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is not EntityNotFoundException entityNotFoundException)
+        {
+            return false;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = "Not Found",
+            Status = StatusCodes.Status404NotFound,
+            Detail = entityNotFoundException.Message,
+            Type = "https://httpstatuses.org/404",
+            Instance = httpContext.Request.Path
+        };
+
+        httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
---
+++ OnlineMed.Api/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineMed.Api.ExceptionHandlers;
+
+internal sealed class GlobalExceptionHandler(
+    IWebHostEnvironment hostEnvironment,
+    IProblemDetailsService problemDetailsService) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Title = "Internal server error",
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = hostEnvironment.IsProduction() ? "An error occurred while processing request." : exception.Message,
+            Type = "https://httpstatuses.org/500",
+            Instance = httpContext.Request.Path
+        };
+
+        await problemDetailsService.WriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            ProblemDetails = problemDetails,
+            Exception = exception
+        }); 
+
+        return true;
+    }
+}
