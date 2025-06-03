a32f7b24cc69438e3fbe0667e148a7c2863ea5ab
Ramadan1011 <shukhratovich75@gmail.com>
2025-06-03 16:50:34 +0500

    feat: add validators

---
+++ OnlineMed.Application/Validators/Appointment/GetAppointmentByIdRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using OnlineMed.Contracts.Requests.Appointment;
+
+namespace OnlineMed.Application.Validators.Appointment;
+
+public sealed class GetAppointmentByIdRequestValidator : AbstractValidator<GetAppointmentByIdRequest>
+{
+    public GetAppointmentByIdRequestValidator()
+    {
+        RuleFor(a => a.Id)
+            .NotEmpty()
+            .WithMessage("Appointment ID is required.")
+            .GreaterThan(0)
+            .WithMessage("Appointment ID must be greater than 0.");
+    }
+}
