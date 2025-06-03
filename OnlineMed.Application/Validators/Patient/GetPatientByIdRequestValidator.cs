e9ad0e9434081b8e3065dde91b1eb0a537c9f674
Ramadan1011 <shukhratovich75@gmail.com>
2025-06-03 11:25:13 +0500

    feat: add validator to patient requests

---
+++ OnlineMed.Application/Validators/Patient/GetPatientByIdRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using OnlineMed.Contracts.Requests.Doctor;
+
+namespace OnlineMed.Application.Validators.Patient;
+
+public sealed class GetPatientByIdRequestValidator : AbstractValidator<GetDoctorByIdRequest>
+{
+    public GetPatientByIdRequestValidator()
+    {
+        RuleFor(d => d.Id)
+            .NotEmpty()
+            .WithMessage("Patient ID is required.")
+            .GreaterThan(0)
+            .WithMessage("Patient ID must be greater than 0.");
+    }
+}
