34132e583f09003d7eeaeb54f61d44778009ac35
Ramadan1011 <shukhratovich75@gmail.com>
2025-06-03 11:23:52 +0500

    feat: create patient responses

---
+++ OnlineMed.Contracts/Responses/Patient/UpdatePatientResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineMed.Contracts.Responses.Patient;
+
+public sealed record UpdatePatientResponse(
+    int Id,
+    string FullName,
+    string Email,
+    string? PhoneNumber,
+    DateTime CreatedAt,
+    DateTime? LastUpdatedAt);
